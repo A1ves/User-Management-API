2bad8b46224f580d534e1daaf889bf3207d0e4fc
Ayron Alves <86841796+A1ves@users.noreply.github.com>
2024-06-07 15:32:46 +0000

    add jwt authenticate

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import { fastify } from 'fastify'
 import { DatabaseMemoryUser } from './database-memory.js'
+import jwt from 'jsonwebtoken'
 
+const secretKey = 'ayron'
 const server = fastify()
 const database = new DatabaseMemoryUser()
 
@@ -27,16 +29,32 @@ server.post('/api/auth/login', (request, reply) => {
     const ID = database.login({ email, password })
 
     if(ID){
-        return reply.status(201).send({ message: 'Login successful' })
+        const token = jwt.sign({ID}, secretKey, { expiresIn: 300 })
+        // return reply.status(201).send({ message: 'Login successful' }).
+        return reply.status(201).send({auth: true, token})
     } else {
         return reply.status(401).send({ error: 'Invalid username or password' });
     }
 })
 
-server.get('/api/auth/users', (request, reply) => {
-    return reply.status(200).send(database.list())
+server.get('/api/auth/users', {
+    preHandler: verifyJWT,
+    handler: async (request, reply) => {
+        return reply.status(200).send(database.list())
+    }
 })
 
+function verifyJWT(request, reply, done){
+    const token = request.headers['x-acess-token']
+    jwt.verify(token, secretKey, (err, decoded) => {
+        if (err) {
+            return reply.status(401).send({ error: 'Unauthorized' });
+        }
+
+        request.ID = decoded.ID;
+        done();
+    });
+}
 
 server.listen({
     port:3333
